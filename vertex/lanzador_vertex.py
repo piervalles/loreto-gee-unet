03834a00caef9713a08e29e4b06d382f59728a9f
Piero Valles <200243539+piervalles@users.noreply.github.com>
2026-05-28 12:05:51 -0500

    feat: entrenar con CPU

--- vertex/lanzador_vertex.py
+++ vertex/lanzador_vertex.py
@@ -4,7 +4,7 @@ from google.cloud import aiplatform
 # 1. CREDENCIALES Y UBICACIÓN
 # =====================================================================
 PROJECT_ID = "proyecto-ia-496311"
-REGION = "us-central1" # La región más barata y con más GPUs disponibles
+REGION = "us-central1" 
 BUCKET_STAGING = "gs://dataset-tfrecords-loreto"
 
 print("Inicializando conexión con Vertex AI...")
@@ -16,13 +16,12 @@ aiplatform.init(project=PROJECT_ID, location=REGION, staging_bucket=BUCKET_STAGI
 print("Empaquetando el código de entrenamiento...")
 
 job = aiplatform.CustomTrainingJob(
-    display_name="entrenamiento-unet-loreto-full",
-    script_path="entrenamiento_masivo.py", # El archivo que creamos en el paso anterior
+    display_name="entrenamiento-unet-loreto-cpu",
+    script_path="entrenamiento_masivo.py", 
     
-    # Usamos un contenedor pre-configurado de Google que ya tiene TensorFlow 2.x optimizado para GPU
-    container_uri="us-docker.pkg.dev/vertex-ai/training/tf-gpu.2-12.py310:latest",
+    # CRÍTICO: Cambiamos al contenedor de CPU oficial de Google
+    container_uri="us-docker.pkg.dev/vertex-ai/training/tf-cpu.2-12.py310:latest",
     
-    # Requisitos de hardware y librerías extra
     requirements=["google-cloud-storage", "numpy"] 
 )
 
@@ -30,14 +29,12 @@ job = aiplatform.CustomTrainingJob(
 # 3. LANZAMIENTO A LA SUPERCOMPUTADORA
 # =====================================================================
 print("\n=== SOLICITANDO HARDWARE A GOOGLE CLOUD ===")
-print("Solicitando: 1 máquina n1-standard-4 con 1 GPU NVIDIA Tesla T4...")
+print("Solicitando: 1 máquina n1-highcpu-16 (16 vCPUs sin GPU)...")
 
-# El script se pausa aquí en tu terminal local, pero el trabajo arranca en la nube
+# Fuerza bruta de procesador, cero bloqueos de cuota
 modelo = job.run(
-    machine_type="n1-standard-4", # 4 vCPUs y 15 GB de RAM
-    accelerator_type="NVIDIA_TESLA_T4", # GPU dedicada para Deep Learning
-    accelerator_count=1,
-    sync=True # True significa que verás los logs del entrenamiento aquí mismo en tu terminal
+    machine_type="n1-highcpu-16", 
+    sync=True 
 )
 
 print("\n=== ¡EL TRABAJO EN VERTEX AI HA CULMINADO! ===")
